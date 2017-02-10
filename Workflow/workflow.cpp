f590458ea7f6b4b20926d3f9774490a7acbe4605
bigzhao <350490072@qq.com>
2017-02-10 10:14:35 +0800

    workflow-v1

--- Workflow/workflow.cpp
+++ Workflow/workflow.cpp
@@ -1,7 +1,79 @@
 #include "workflow.h"
 
 
+/************************************************************************
+* Function Name : calculate_est_and_best
+* Create Date : 2017/2/8
+* Author/Corporation : bigzhao
+**
+Description : 计算有向图的est 及反过来的 best
+*
+* Param :*nodes：节点数组
+* nodes_length: 节点的数量
+**
+************************************************************************/
+double calculate_est_and_best(struct node *nodes, int nodes_length)
+{
+	int i, j, min_index;
+	struct ant a, backward_a;
+	double min_makespan;
 
+	// 为蚂蚁的 solution[nodes_length] 申请内存
+	a.solutions = (struct solution *)malloc(sizeof(struct solution) * nodes_length);
+	if (a.solutions == NULL) {
+		fprintf(stderr, "ants initialize failed.");
+		exit(1);
+	}
+	backward_a.solutions = (struct solution *)malloc(sizeof(struct solution) * nodes_length);
+	if (backward_a.solutions == NULL) {
+		fprintf(stderr, "ants initialize failed.");
+		exit(1);
+	}
+
+	for (i = 0; i < nodes_length; i++) {
+		min_index = 0;
+		for (j = 1; j < nodes[i].num_instances; j++) {
+			if (nodes[i].instances[j].time < nodes[i].instances[min_index].time)
+				min_index = j;
+		}
+		a.solutions[i].inst = &nodes[i].instances[min_index];
+		backward_a.solutions[i].inst = &nodes[i].instances[min_index];
+	}
+	time_evaluation(&a, nodes, nodes_length);
+	for (i = 0; i < nodes_length; i++) {
+		nodes[i].est = a.solutions[i].st;
+	}
+	min_makespan = a.makespan;
+	backward_time_evaluation(&backward_a, nodes, nodes_length);
+	for (i = 0; i < nodes_length; i++) {
+		nodes[i].best = backward_a.solutions[i].st;
+	}
+	printf("\n EST:");
+	for (i = 0; i < nodes_length; i++) {
+		printf(" %lf ", nodes[i].est);
+	}
+	printf("\n");
+	printf("\n BEST:");
+	for (i = 0; i < nodes_length; i++) {
+		printf(" %lf ", nodes[i].best);
+	}
+	printf("\n");
+	free(a.solutions);
+	free(backward_a.solutions);
+	return min_makespan;
+}
+
+/************************************************************************
+* Function Name : calculate_tau0
+* Create Date : 2017/2/8（注释时间）
+* Author/Corporation : bigzhao
+**
+Description : 计算信息素的初始值，ε0，这里使用的是贪心取得的值
+*
+* Param :*nodes：节点数组
+* nodes_length: 节点的数量
+**
+************************************************************************/
 double calculate_tau0(struct node *nodes, int nodes_length, double *max_cost, double *min_cost)
 {
 	int i, j;
@@ -11,20 +83,20 @@ double calculate_tau0(struct node *nodes, int nodes_length, double *max_cost, do
 	*min_cost = 0.0;
 
 	for (i = 0; i < nodes_length; i++) {
-		max_index = 0, min_index = 0;
-		for (j = 1; j < nodes[i].num_instances; j++) {
-			if (nodes[i].instances[j].cost < nodes[i].instances[min_index].cost)
-				min_index = j;
-			if (nodes[i].instances[max_index].cost < nodes[i].instances[j].cost)
-				max_index = j;
-		}
-		(*max_cost) += nodes[i].instances[max_index].cost;
-		(*min_cost) += nodes[i].instances[min_index].cost;
+		//max_index = 0, min_index = 0;
+		//for (j = 1; j < nodes[i].num_instances; j++) {
+		//	if (nodes[i].instances[j].cost < nodes[i].instances[min_index].cost)
+		//		min_index = j;
+		//	if (nodes[i].instances[max_index].cost < nodes[i].instances[j].cost)
+		//		max_index = j;
+		//}
+		(*max_cost) += nodes[i].max_cost;
+		(*min_cost) += nodes[i].min_cost;
 	}
 	return (*min_cost) / (*max_cost);
 }
 
-void ants_initialize(struct ant *a, int nodes_length)
+void ants_apply_memory(struct ant *a, int nodes_length)
 {
 	int i;
 
@@ -72,64 +144,127 @@ Description : 蚂蚁的解的构造，按概率来操作，小于Q则挑选最大的，大于Q则
 * pheromone：信息素矩阵
 **
 ************************************************************************/
-void solution_construction(struct ant *a, struct node *nodes, int nodes_length)
+void solution_construction(struct ant *a, struct node *nodes, int nodes_length, double time_constraint, double min_makespan)
 {
-	int i, j, k, max_index, max_instance_index = 0, min_instance_index = 0;
-	double q, sum = 0.0, probability, base;
-	double eta, eta_c, eta_t;
+	int i, j, max_index;
+	double q, sum = 0.0, probability, base, avg_min_time, SD, MAT;
+	double *eta, eta_c, eta_t;
 	for (i = 0; i < nodes_length; i++) {
 		q = u01();
-		max_index = 0;
-		for (j = 1; j < nodes[i].num_instances; j++) {
-			if (nodes[i].instances[max_instance_index].cost < nodes[i].instances[j].cost)
-				max_instance_index = j;
-			if (nodes[i].instances[min_instance_index].cost < nodes[i].instances[j].cost)
-				min_instance_index = j;
-		}
-
+		eta = (double *)malloc(nodes[i].num_instances * sizeof(double));
+		avg_min_time = calculate_avg_min_time(nodes[i]);
+		SD = avg_min_time * time_constraint / min_makespan;
+		MAT = max(abs(nodes[i].max_time - SD), abs(SD - nodes[i].min_time));
 		if (q < Q) {
+			max_index = 0;
+			eta_c = (nodes[i].max_cost - nodes[i].instances[0].cost + 1) /
+				(nodes[i].max_cost - nodes[i].min_cost + 1);
+			//if (nodes[i].instances[0].time <= SD)
+			//	eta_t = 1;
+			//else
+				eta_t = (MAT - abs(nodes[i].instances[0].time - SD) + 1) / (MAT + 1);
+			eta[0] = 0.5 * (eta_c + eta_t);
 			for (j = 1; j < nodes[i].num_instances; j++) {
-				eta_c = (nodes[i].instances[max_instance_index].cost - nodes[i].instances[j].cost + 1) /
-					(nodes[i].instances[max_instance_index].cost - nodes[i].instances[min_instance_index].cost + 1);
-				if (nodes[i].instances[max_index].pheromone * pow(1 / eta_c, BETA)
-					< nodes[i].instances[j].pheromone * pow(1 / eta_c, BETA))
+				// 计算 eta
+				eta_c = (nodes[i].max_cost - nodes[i].instances[j].cost + 1) /
+					(nodes[i].max_cost - nodes[i].min_cost + 1);
+				//if (nodes[i].instances[j].time <= SD)
+				//	eta_t = 1;
+				//else
+					eta_t = (MAT - abs(nodes[i].instances[j].time - SD) + 1) / (MAT + 1);
+				eta[j] = 0.5 * (eta_c + eta_t);
+
+				if (nodes[i].instances[max_index].pheromone * pow(BETA, eta[max_index])
+					< nodes[i].instances[j].pheromone * pow(BETA, eta[j]))
 					max_index = j;
 			}
+
+			a->solutions[i].inst = &nodes[i].instances[max_index];
 		}
 		else {
 			//否则就赌轮盘
 			sum = 0.0;
-			for (j = 0; j < CITY_NUM; j++) {
-				if (1 == remain_city[j])
-					continue;
-				sum += pheromone[a->city[i - 1]][j] * pow(1.0 / distance[a->city[i - 1]][j], BETA);
-				city_not_be_selected[length_city_not_be_selected++] = j;
+			for (j = 0; j < nodes[i].num_instances; j++) {
+				// 计算 eta
+				eta_c = (nodes[i].max_cost - nodes[i].instances[j].cost + 1) /
+					(nodes[i].max_cost - nodes[i].min_cost + 1);
+				//if (nodes[i].instances[j].time <= SD)
+				//	eta_t = 1;
+				//else
+					eta_t = (MAT - abs(nodes[i].instances[j].time - SD) + 1) / (MAT + 1);
+				eta[j] = 0.5 * (eta_c + eta_t);
+				sum += nodes[i].instances[j].pheromone * pow(BETA, eta[j]);
 			}
 
 			probability = u01();
 
 			base = 0.0;
-			for (j = 0; j < length_city_not_be_selected; j++) {
-				base += (pheromone[a->city[i - 1]][city_not_be_selected[j]]
-					* pow(1.0 / distance[a->city[i - 1]][city_not_be_selected[j]], 2) / sum);
+			for (j = 0; j < nodes[i].num_instances; j++) {
+				base += nodes[i].instances[j].pheromone * pow(BETA, eta[j]) / sum;
 				if (probability < base) {
-					a->city[i] = city_not_be_selected[j];
-					remain_city[city_not_be_selected[j]] = 1;
+					a->solutions[i].inst = &nodes[i].instances[j];
 					break;
 				}
 			}
 		}
+		free(eta);
 	}
-	a->fitness = fitness_evaluation(a->city, distance);
+	//for (i = 0; i < nodes_length; i++)
+	//	printf(" %d ", a->solutions[i].inst->id);
+	//printf("\n");
+}
+
+/************************************************************************
+* Function Name : local_update_pheromone
+* Create Date : 2017/2/7
+* Author/Corporation : bigzhao
+**
+Description : 局部信息素更新
+*
+* Param : a: 需要更新的蚂蚁
+* pheromone: 信息素矩阵
+* tau0：初始值
+**
+************************************************************************/
+void local_update_pheromone(struct ant *a,  double tau0, int nodes_length)
+{
+	int i;
+	for (i = 0; i < nodes_length; i++) {
+		a->solutions[i].inst->pheromone *= (1 - ROU);
+		a->solutions[i].inst->pheromone += ROU * tau0;
+	}
+}
+
+/************************************************************************
+* Function Name : global_update_pheromone
+* Create Date : 2017/1/16
+* Author/Corporation : bigzhao
+**
+Description : 全局信息素更新，全局更新只针对于最好的蚂蚁
+*
+* Param : best_ant: 需要更新的蚂蚁
+* pheromone: 信息素矩阵
+**
+************************************************************************/
+void global_update_pheromone(struct ant *best_ant, int nodes_length)
+{
+	int i;
+	int city_index = 0;
+	for (i = 0; i < nodes_length; i++) {
+		best_ant->solutions[i].inst->pheromone *= (1 - ALPHA);
+		best_ant->solutions[i].inst->pheromone += ALPHA * best_ant->score;
+	}
 }
 
 int main() {
 	struct ant ants[POP];    // 全局蚁群
 	struct ant best_ant;     // 最优秀蚂蚁
 	struct node *nodes = NULL;
-	double time_constraint, tau0, max_cost, min_cost;
-	int nodes_length, i, j, iter, ant_index;
-	nodes = read_data("9.txt", &nodes_length, &time_constraint);
+	double time_constraint, tau0, max_cost, min_cost, min_makespan;
+	int nodes_length, i, j, iter, best_ant_index;
+
+
+	nodes = read_data("j1201_1.txt", &nodes_length, &time_constraint);
 	//for (i = 0; i < nodes_length; i++) {
 	//	printf("id: %d\n", nodes[i].id);
 	//	for (j = 0; j < nodes[i].num_instances; j++) {
@@ -148,21 +283,50 @@ int main() {
 	
 	tau0 = calculate_tau0(nodes, nodes_length, &max_cost, &min_cost);
 	//printf("%lf", tau0);
+	min_makespan = calculate_est_and_best(nodes, nodes_length);
 	pheromone_initialize(nodes,nodes_length, tau0);  //tau0要怎么算？
-	ants_initialize(ants, nodes_length);
+	ants_apply_memory(ants, nodes_length);
+	best_ant.solutions = (struct solution *)malloc(sizeof(struct solution) * nodes_length);
+	
 	for (iter = 0; iter < ITERATION; iter++) {
-		for (i = 0; i < nodes_length; i++) {
-			ants[0].solutions[i].n = &nodes[i];
-			ants[0].solutions[i].inst = &nodes[i].instances[0];
-		}
-
+		//printf("pheromone:");
+		//for (i = 0; i < nodes_length; i++) {
+		//	for (j = 0; j < nodes[i].num_instances; j++)
+		//		printf(" %lf ", nodes[i].instances[j].pheromone);
+		//	printf("\n");
+		//}
 		for (i = 0; i < POP; i++) {
+			solution_construction(ants + i, nodes, nodes_length, time_constraint, min_makespan);
 			time_evaluation(ants + i, nodes, nodes_length);
+			cost_evaluation(ants + i, nodes, nodes_length);
 			score_evaluation(ants + i, time_constraint, min_cost, max_cost);
+			local_update_pheromone(&ants[i], tau0, nodes_length);
+
 		}
+		//ants[0].solutions[0].inst->pheromone = 0;
+		//printf("asdadadad:%lf\n", ants[0].solutions[0].inst->pheromone);
+
+		best_ant_index = find_the_best_ant(ants);
+		if (iter == 0 || best_ant.score < ants[best_ant_index].score) {
+			memcpy(best_ant.solutions, ants[best_ant_index].solutions, sizeof(solution) * nodes_length);
+			best_ant.cost = ants[best_ant_index].cost;
+			best_ant.score = ants[best_ant_index].score;
+			best_ant.makespan = ants[best_ant_index].makespan;
+
+		}
+		global_update_pheromone(&best_ant, nodes_length);
+
 	}
-	//for (i = 0; i < nodes_length; i++)
-	//	printf("%lf\n", ants[0].solutions[i].ct);
+
+	printf("makespan: %lf, cost: %lf\n", best_ant.makespan, best_ant.cost);
+	for (i = 0; i < nodes_length; i++) {
+		printf("%d:%d %lf %lf %lf\n", i, best_ant.solutions[i].inst->id, best_ant.solutions[i].inst->time, best_ant.solutions[i].st, best_ant.solutions[i].ct);
+	}
+	//printf("makespan: %lf, cost: %lf\n", ants[best_ant_index].makespan, ants[best_ant_index].cost);
+	//for (i = 0; i < nodes_length; i++) {
+	//	printf("%d:%d %lf %lf %lf\n", i, ants[best_ant_index].solutions[i].inst->id, ants[best_ant_index].solutions[i].inst->time, ants[best_ant_index].solutions[i].st, ants[best_ant_index].solutions[i].ct);
+	//}
+	free(best_ant.solutions);
 	free_memory(nodes, nodes_length, ants);
 	return 0;
 }
--- Workflow/workflow.h
+++ Workflow/workflow.h
@@ -2,12 +2,13 @@
 #define __WORKFLOW__
 
 #define ROU       0.1     // ρ
-#define POP       1      // 蚁群的种群大小
-#define ITER      2500    // 迭代次数
-#define BETA      2       // β
+#define POP       30      // 蚁群的种群大小
+#define BETA      1.2      // β
 #define Q         0.9     // 概率
 #define ALPHA     0.1     // α
-#define ITERATION 100
+#define ITERATION 400
+
+#define max(a,b)         (((a) > (b)) ? (a) : (b))
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,6 +37,9 @@ struct node {
 	int num_succ = 0;
 	int num_pred = 0;
 	int id;
+	double est;
+	double best;
+	double max_time, min_time, max_cost, min_cost;
 };
 
 struct instance {
@@ -53,6 +57,30 @@ struct solution {
 	double ct;
 };
 
+
+/************************************************************************
+* Function Name : find_the_best_ant
+* Create Date : 2017/1/16
+* Author/Corporation : bigzhao
+**
+Description : 用遍历的方法找蚁群里面最优秀的蚂蚁
+*
+* Param : *a: 蚁群
+* return: 最优的下标
+**
+************************************************************************/
+int find_the_best_ant(struct ant *a)
+{
+	int i, best_ant_index = 0;
+
+	for (i = 1; i < POP; i++) {
+		if (a[best_ant_index].score < a[i].score)
+			best_ant_index = i;
+	}
+	return best_ant_index;
+}
+
+
 void score_evaluation(struct ant *a, double time_constraint, double min_cost, double max_cost)
 {
 	if (a->makespan <= time_constraint)
@@ -61,6 +89,37 @@ void score_evaluation(struct ant *a, double time_constraint, double min_cost, do
 		a->score = time_constraint / a->makespan + min_cost / max_cost;
 }
 
+/************************************************************************
+* Function Name : cost_evaluation
+* Create Date : 2017/2/7
+* Author/Corporation : bigzhao
+**
+Description : 测量cost
+*
+* Param :
+* :
+**
+************************************************************************/
+void cost_evaluation(struct ant *a, struct node *nodes, int nodes_length)
+{
+	int i;
+	double cost = 0;
+	for (i = 0; i < nodes_length; i++) {
+		cost += a->solutions[i].inst->cost;
+	}
+	a->cost = cost;
+}
+/************************************************************************
+* Function Name : time_evaluation
+* Create Date : 2017/2/7
+* Author/Corporation : bigzhao
+**
+Description : 测量时间，正常方向
+*
+* Param :
+* :
+**
+************************************************************************/
 void time_evaluation(struct ant *a, struct node *nodes, int nodes_length)
 {
 	std::queue<struct node> ready_queue;
@@ -103,11 +162,100 @@ void time_evaluation(struct ant *a, struct node *nodes, int nodes_length)
 	}
 	// 给蚂蚁a 赋 makespan
 	a->makespan = a->solutions[max_index].ct;
+	//printf("%lf\n", a->makespan);
+}
+
+/************************************************************************
+* Function Name : backward_time_evaluation
+* Create Date : 2017/2/7
+* Author/Corporation : bigzhao
+**
+Description : 测量反方向的时间，也就是论文里的BEST，与上面函数不一样的地方
+在于这里的pred和succ颠倒即可
+*
+* Param :
+* :
+**
+************************************************************************/
+void backward_time_evaluation(struct ant *a, struct node *nodes, int nodes_length)
+{
+	std::queue<struct node> ready_queue;
+	int i, max_index;
+	struct node operating_node;
+
+	for (i = 0; i < nodes_length; i++) {
+		if (0 == nodes[i].num_succ)
+			ready_queue.push(nodes[i]);
+	}
+
+	while (!ready_queue.empty()) {
+		operating_node = ready_queue.front();
+		if (0 == operating_node.num_succ)
+			a->solutions[operating_node.id].st = 0;
+		else {
+			max_index = 0;
+			for (i = 1; i < operating_node.num_succ; i++) {
+				if (a->solutions[operating_node.succ[max_index]->id].ct < a->solutions[operating_node.succ[i]->id].ct) {
+					max_index = i;
+				}
+			}
+			a->solutions[operating_node.id].st = a->solutions[operating_node.succ[max_index]->id].ct;
+		}
+
+		a->solutions[operating_node.id].ct = a->solutions[operating_node.id].st + a->solutions[operating_node.id].inst->time;
+		ready_queue.pop();
+		for (i = 0; i < operating_node.num_pred; i++)
+			ready_queue.push(*(operating_node.pred[i]));
+		//operating_node.succ;
+		//}
+
+	}
+
+	//max_index = 0;
+	//for (i = 1; i < nodes_length; i++) {
+	//	if (a->solutions[max_index].ct < a->solutions[i].ct)
+	//		max_index = i;
+	//	//printf("%lf  ", a->solutions[i].ct);
+	//}
+	//// 给蚂蚁a 赋 makespan
+	//a->makespan = a->solutions[max_index].ct;
+}
+
+double calculate_avg_min_time(struct node n)
+{
+	int min_est_succ_index = 0, min_best_pred_index = 0;
+	int i;
+	if (n.num_succ == 0) {
+		for (i = 1; i < n.num_pred; i++) {
+			if (n.pred[i]->best < n.pred[min_best_pred_index]->best)
+				min_best_pred_index = i;
+		}
+		return (n.pred[min_best_pred_index]->best - n.best);
+	}
+
+	if (n.num_pred == 0) {
+		for (i = 1; i < n.num_succ; i++) {
+			if (n.succ[i]->est < n.succ[min_est_succ_index]->est)
+				min_est_succ_index = i;
+		}
+		return (n.succ[min_est_succ_index]->est - n.est);
+	}
+
+	for (i = 1; i < n.num_succ; i++) {
+		if (n.succ[i]->est < n.succ[min_est_succ_index]->est)
+			min_est_succ_index = i;
+	}
+	for (i = 1; i < n.num_pred; i++) {
+		if (n.pred[i]->best < n.pred[min_best_pred_index]->best)
+			min_best_pred_index = i;
+	}
+	return (n.succ[min_est_succ_index]->est - n.est + n.pred[min_best_pred_index]->best - n.best) / 2.0;
 }
 
 struct node * read_data(char *file_name, int *num_nodes, double *time_constraint) {
 	FILE *fp;
 	int num_node, num_edges, i, j, temp, current, next;
+	int max_cost_index, min_cost_index, max_time_index, min_time_index;
 	double temp1;
 	struct node *nodes;
 	fp = fopen(file_name, "r");
@@ -133,44 +281,32 @@ struct node * read_data(char *file_name, int *num_nodes, double *time_constraint
 		nodes[current - 1].num_succ++;
 		nodes[next - 1].pred[nodes[next - 1].num_pred] = &nodes[current - 1];
 		nodes[next - 1].num_pred++;
-		//if (NULL = nodes[current].succ)
-		//	nodes[current].succ++;
-		//if (nodes[current - 1].num_succ == 0) {
-		//	nodes[current - 1].succ = (struct node**)malloc(sizeof(struct node*));
-		//	nodes[current - 1].succ[0] = &nodes[next - 1];
-		//	nodes[current - 1].num_succ++;
-		//}
-		//else
-		//{
-		//	nodes[current - 1].num_succ++;
-		//	nodes[current - 1].succ = (struct node**)realloc(nodes[current - 1].succ, sizeof(struct node*));
-		//	nodes[current - 1].succ[nodes[current - 1].num_succ - 1] = &nodes[next - 1];
-		//}
-		//if (NULL != nodes[current].pred)
-		//	nodes[current].pred++;
-		//if (nodes[current - 1].num_pred == 0) {
-		//	nodes[current - 1].pred = (struct node**)malloc(sizeof(struct node*));
-		//	nodes[next - 1].pred[0] = &nodes[current - 1];
-		//	nodes[next - 1].num_pred++;
-		//}
-		//else
-		//{
-		//	nodes[current - 1].num_pred++;
-		//	nodes[current - 1].pred = (struct node**)realloc(nodes[current - 1].pred, sizeof(struct node*) * nodes[current - 1].num_pred);
-		//	nodes[current - 1].pred[nodes[current - 1].num_pred - 1] = &nodes[next - 1];
-		//}
 	}
 
 	for (i = 0; i < num_node; i++) {
 		fscanf(fp, "%d %d", &temp, &nodes[i].num_instances);
-		printf("%d %d\n", temp, nodes[i].num_instances);
-
 		nodes[i].instances = (struct instance *) malloc(sizeof(struct instance) * nodes[i].num_instances);
-
+		max_cost_index = 0; min_cost_index = 0; max_time_index = 0; min_time_index = 0;
 		for (j = 0; j < nodes[i].num_instances; j++) {
 			fscanf(fp, "%d %lf %lf %lf", &nodes[i].instances[j].id,
 				&nodes[i].instances[j].reliability, &nodes[i].instances[j].time, &nodes[i].instances[j].cost);
+			if (0 == j)
+				continue;     //因为下面四个index默认值为0 所以没必要再去判断
+			if (nodes[i].instances[max_cost_index].cost < nodes[i].instances[j].cost)
+				max_cost_index = j;
+			if (nodes[i].instances[j].cost < nodes[i].instances[min_cost_index].cost)
+				min_cost_index = j;
+			if (nodes[i].instances[max_time_index].time < nodes[i].instances[j].time)
+				max_time_index = j;
+			if (nodes[i].instances[j].time < nodes[i].instances[min_time_index].time)
+				min_time_index = j;
 		}
+		// 赋值
+		nodes[i].max_time = nodes[i].instances[max_time_index].time;
+		nodes[i].min_time = nodes[i].instances[min_time_index].time;
+		nodes[i].max_cost = nodes[i].instances[max_cost_index].cost;
+		nodes[i].min_cost = nodes[i].instances[min_cost_index].cost;
+		printf("/n %lf %lf %lf %lf \n", nodes[i].max_cost, nodes[i].min_cost, nodes[i].max_time, nodes[i].min_time);
 	}
 	fscanf(fp, "%lf %lf %lf", &temp1, time_constraint, &temp1);
 	fclose(fp);
